9ab9accb6218af9024e857094ddee26689591ace
Prashanth Chaduvala <prashanth.chaduvala@cdatechindia.com>
2026-06-20 13:39:39 +0530

    Auth service initial setup with health endpoint

---
+++ services/auth-service/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HealthView(APIView):
+    def get(self, request):
+        return Response({
+            "service": "auth-service",
+            "status": "running"
+        })
